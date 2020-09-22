015e7c43ab44cdd2f863626de3e21ea65802c3e8
Di-ref <belgaidferid@gmail.com>
2020-09-22 11:24:05 +0100

    Updates

---
+++ precache-manifest.66ce2c93a4e9903153199f10a875b36f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f9da89017c49d71b4dc47773506b2a",
+    "url": "/diref/index.html"
+  },
+  {
+    "revision": "7b32e149cdc0400537aa",
+    "url": "/diref/static/css/main.73ded73d.chunk.css"
+  },
+  {
+    "revision": "0a1c210cac8c1658efa2",
+    "url": "/diref/static/js/2.189d71b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/diref/static/js/2.189d71b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b32e149cdc0400537aa",
+    "url": "/diref/static/js/main.47bc3aee.chunk.js"
+  },
+  {
+    "revision": "2b994630202eaa26107c",
+    "url": "/diref/static/js/runtime-main.507c9f6e.js"
+  },
+  {
+    "revision": "29a15ef22c799c8dbabcbfb00be2a1ea",
+    "url": "/diref/static/media/streettrees.29a15ef2.jpg"
+  }
+]);
